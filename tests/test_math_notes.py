2fe60d0daa680605275d043d1b2053c08aa71f16
Jaime Lopez <jaime.meriz13@gmail.com>
2021-08-15 12:27:27 -0500

    code coverage basic files

--- tests/test_math_notes.py
+++ tests/test_math_notes.py
@@ -2,13 +2,10 @@
 
 """Tests for `math_notes` package."""
 
-
 import unittest
 from click.testing import CliRunner
 
-from math_notes import math_notes
-from math_notes import cli
-
+import math_notes 
 
 class TestMath_notes(unittest.TestCase):
     """Tests for `math_notes` package."""
@@ -20,14 +17,4 @@ class TestMath_notes(unittest.TestCase):
         """Tear down test fixtures, if any."""
 
     def test_000_something(self):
-        """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'math_notes.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        """Test something.""
