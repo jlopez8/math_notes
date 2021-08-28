a8c264dbc25415c63f89b597617409d78f4e8048
Jaime Lopez <jaime.meriz13@gmail.com>
2021-08-28 11:48:05 -0500

    fix with black and flake8

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,10 +2,12 @@ from math_notes.configs import config
 
 from pathlib import Path
 
+
 def test_paths():
     """Test that paths exist."""
-    assert Path.exists(config.app_id_path) == True
-    assert Path.exists(config.app_key_path) == True
+    assert Path.exists(config.app_id_path)
+    assert Path.exists(config.app_key_path)
+
 
 def test_not_empty_keys():
     """Test that keys loaded are not empty."""
