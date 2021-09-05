16efb08b850d68970fea62fe6cc29d94396db484
Jaime Lopez <jaime.meriz13@gmail.com>
2021-09-04 19:11:52 -0500

    finalize writing  out test_backend

--- math_notes/backend.py
+++ math_notes/backend.py
@@ -1,18 +1,21 @@
+import csv
+
 from tkinter import Tk, Canvas, ttk, Button
 from tkinter import constants as con
 
-import .predict.predict
+from math_notes import predict
 
-def save():
-    """Saves a canvas file after appending with string of 
+
+def save(canvas_image, filename="tests/temp_canvas_img.png"):
+    """Saves a canvas file after appending with string of
     random characters for uniquness.
     """
-    filename = rnd_image_filename()
     canvas_image.save(filename)
-    
+
+
 def paint(event):
-    """Places a dot at each mouse location corresponding to the click event. 
-    On the draw object, adds a line connection nearby locations of where the 
+    """Places a dot at each mouse location corresponding to the click event.
+    On the draw object, adds a line connection nearby locations of where the
     event occurs. This latter image is saved and represents the image drawn
     on the tkinter canvas by the user.
 
@@ -22,7 +25,8 @@ def paint(event):
     x1, y1 = (event.x - 1), (event.y - 1)
     x2, y2 = (event.x + 1), (event.y + 1)
     cv.create_oval(x1, y1, x2, y2, fill="black", width=2)
-    draw.line([x1, y1, x2, y2], fill="black", width=2)    
+    draw.line([x1, y1, x2, y2], fill="black", width=2)
+
 
 def save_posn(event):
     """Saves positional coordinates of object event.
@@ -32,58 +36,66 @@ def save_posn(event):
     """
     global lastx, lasty
     lastx, lasty = event.x, event.y
-    
+
+
 def add_line(event):
     """Adds a line connection previous location of event to current event location.
-    Event here is where the mouse was and is located. 
+    Event here is where the mouse was and is located.
 
     :param event: Recorded mouse-click events for drawing.
     :type event: class tkinter.Event
     """
     canvas.create_line((lastx, lasty, event.x, event.y))
     save_posn(event)
-    
+
+
 def save_predictions(predictions):
     """Save a list of LaTeX predictions to a csv.
-    
+
     :param predictions: A list of strings of the latex predictions.
     :type predictions: List[str]
-    
+
     :return: none
     :rtype: none
     """
     headers = ["latex"]
-    with open('outputs.csv', 'w', newline='') as file:
+    with open("outputs.csv", "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(headers)   
+        writer.writerow(headers)
         for i in range(len(predictions)):
             latex = predictions[i]
             writer.writerow(latex)
 
         file.close()
 
-def user_input():
-    """Prompts the user to decide how they would like to provide an image to the math OCR. 
-    They can either write on a canvas and save the file for predictions or 
+
+def user_input(user_choice=3):
+    """Prompts the user to decide how they would like to provide an image to the math OCR.
+    They can either write on a canvas and save the file for predictions or
     specify a directory with multiple images
-    
+
     :param width: Canvas width, defaults to 800.
     :type width: int, optional
-    
-    :return: Path containing images for prediction. 
+
+    :return: Path containing images for prediction.
     :rtype: str
     """
-
-    user_choice = int(input("Enter '1' for receiving a handraw canvas or '2' provide an image path for Math OCR or '3' to exit: \n"))
+    if not bool(user_choice):
+        user_choice = int(
+            input(
+                "Enter '1' for receiving a handraw canvas or '2' provide an image path for Math OCR or '3' to exit: \n"
+            )
+        )
 
     if user_choice == 1:
         pilot_canvas()
         path = "./temp_files"
-    elif user_choice==2:
+    elif user_choice == 2:
         path = input("Provide image directory: ")
     elif user_choice == 3:
-        path = "" 
+        path = ""
     return path
-    
+
+
 def quit():
     root.destroy()
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,30 +1,38 @@
+import os
 from pathlib import Path
 
-from math_notes.predict import predict
+import csv
 
+from math_notes import predict, backend
+
+from tkinter import Tk, Canvas
 from PIL import ImageGrab, ImageTk, ImageDraw
 import PIL
 
-def test_save():
-    filename = "tests/test_save.png"
-    my_file = Path(filename)
-    canvas_image = PIL.Image.new("RGB", (600, 400), (255, 255, 255))
-    canvas_image.save(filename)
-    
-    assert my_file.is_file()
+width = 600
+height = 400
+center = height // 2
+white = (255, 255, 255)
 
-    
-    
-    >> # delete the image in this case
-    
-# def test_paint():
-    
-# def test_save_posn():
-    
-# def test_add_line():
-    
-# def test_save_predictions():
-    
-# def test_user_input():
-    
-    
+
+def test_save():
+    filename = "tests/temp_canvas_img.png"
+    canvas_image = PIL.Image.new("RGB", (width, height), white)
+    backend.save(canvas_image, filename=filename)
+    my_file = Path(filename)
+    assert my_file.is_file()
+    os.remove(my_file)
+
+
+def test_save_predictions():
+    predictions = ["x", "\\int_0^\\intfy 1/x^2 dx"]
+    backend.save_predictions(predictions)
+    filename = "outputs.csv"
+    my_file = Path(filename)
+    os.remove(my_file)
+
+
+def test_user_input():
+    path = backend.user_input(user_choice=3)
+    print("path is path:", path)
+    assert "" == path
