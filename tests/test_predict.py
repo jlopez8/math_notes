f5703c7deee609c78b61138d612c510c996236b1
jaime_lopez <jaime.meriz13@gmail.com>
2021-12-25 16:49:07 -0600

    Development git tests (#19)
    * run black
    
    * rearranged where pytests fixtures are called to focus on prediction only
    
    * update cfg_setup_teardown fixture to run from one fixture
    
    * tests for correct up and teardown of actual keys

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,14 +1,51 @@
 import os
 from pathlib import Path
-
 import base64
+import shutil
 
-from tests import cfg_setup, cfg_teardown
+import pytest
+
 from math_notes import predict
 
-    
-def test_prediction_single(cfg_setup):
-    
+
+@pytest.fixture(scope="module")
+def cfg_setup_teardown():
+
+    # For now this is in configs.
+    path = Path("./configs/")
+
+    # Setup.
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    filename = "app_key.txt"
+    filepath = path / filename
+    mode = "w+"
+    file_object = open(filepath, mode)
+    for i in range(1):
+        file_object.write("app_dummy_email_dot_com")
+    file_object.close()
+
+    filename = "app_id.txt"
+    filepath = path / filename
+    mode = "w+"
+    file_object = open(filepath, mode)
+    for i in range(1):
+        file_object.write("ABCDEFGHIJKLMNOP1234567890")
+    file_object.close()
+
+    yield
+
+    # For now this is in configs.
+    path = Path("./configs/")
+
+    # Teardown.
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+
+
+def test_prediction_single(cfg_setup_teardown):
+
     filename = "tests/data/integral_to_transform_1.png"
     true_latex_string = "f(x)=\\int_{a}^{x} t^{3}+1 d t"
 
@@ -22,7 +59,7 @@ def test_prediction_single(cfg_setup):
     assert latex_return[0] == true_latex_string
 
 
-def test_prediction_multiple(cfg_setup):
+def test_prediction_multiple(cfg_setup_teardown):
 
     filenames = [
         "tests/data/integral_to_transform_1.png",
@@ -41,9 +78,3 @@ def test_prediction_multiple(cfg_setup):
     latex_returns = predict._predict(image_uri)
 
     assert latex_returns == true_latex_strings
-
-
-def test_call_teardown(cfg_teardown):
-    assert True == True
-    pass
-    
