3cb9efb87bdd884c15e07c077fc321c51547122a
Jaime Lopez <jaime.meriz13@gmail.com>
2021-12-26 13:37:19 -0600

    playing with configs

--- math_notes/config.py
+++ math_notes/config.py
@@ -1,18 +1,9 @@
-import os
 from pathlib import Path
 
 
 # Read the app_id and app_key required for MathPix API.
-APP_ID = Path("")
-APP_KEY = Path("")
+app_id_path = Path("configs/app_id.txt")
+app_key_path = Path("configs/app_key.txt")
 
-if "PYTEST_CURRENT_TEST" in os.environ:
-    APP_ID = os.environ["APP_ID"]
-    APP_KEY = os.environ["APP_KEY"]
-
-else:
-    app_id_path = Path("configs/app_id.txt")
-    app_key_path = Path("configs/app_key.txt")
-
-    APP_ID = app_id_path.read_text()
-    APP_KEY = app_key_path.read_text()
+APP_ID = app_id_path.read_text()
+APP_KEY = app_key_path.read_text()
