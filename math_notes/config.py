e972b5f4439b907fed618cfcaf1b6b7253d1d72e
Jaime Lopez <jaime.meriz13@gmail.com>
2021-12-26 12:54:23 -0600

    update pytest configs for secret keys 1

--- math_notes/config.py
+++ math_notes/config.py
@@ -1,9 +1,18 @@
+import os
 from pathlib import Path
 
 
 # Read the app_id and app_key required for MathPix API.
-app_id_path = Path("configs/app_id.txt")
-app_key_path = Path("configs/app_key.txt")
+APP_ID = ""
+APP_KEY = ""
 
-APP_ID = app_id_path.read_text()
-APP_KEY = app_key_path.read_text()
+if "PYTEST_CURRENT_TEST" in os.environ:
+    APP_ID = os.environ['APP_ID']
+    APP_KEY = os.environ['APP_KEY']
+
+else:
+    app_id_path = Path("configs/app_id.txt")
+    app_key_path = Path("configs/app_key.txt")
+
+    APP_ID = app_id_path.read_text()
+    APP_KEY = app_key_path.read_text()
